7cb368558ed22d43d2ea7e7cca7ec79676316cfa
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-14 01:09:19 +0530

    feat: header section implemented

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import search from './search.png';
 import basket from './basket.png';
+import headerImg from './headerImg.jpg';
 
 export const assets = {
     logo,
     search,
     basket,
+    headerImg,
 };
