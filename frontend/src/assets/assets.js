e38ecb96ebaca235b7891cd113db98e7efe144b5
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-14 10:38:46 +0530

    feta: explore menu section implemented

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,9 +3,43 @@ import search from './search.png';
 import basket from './basket.png';
 import headerImg from './headerImg.jpg';
 
+import noodles from './noodles.jpg';
+import pizza from './pizza.jpg';
+import cake from './cake.jpg';
+import roti from './roti.jpg';
+import pasta from './pasta.jpg';
+import salad from './salad.jpg';
+
 export const assets = {
     logo,
     search,
     basket,
     headerImg,
 };
+
+export const menu_list = [
+    {
+        menu_name: 'Noodles',
+        menu_image: noodles
+    },
+    {
+        menu_name: 'Pizza',
+        menu_image: pizza
+    },
+    {
+        menu_name: 'Cake',
+        menu_image: cake
+    },
+    {
+        menu_name: 'Roti',
+        menu_image: roti
+    },
+    {
+        menu_name: 'Pasta',
+        menu_image: pasta
+    },
+    {
+        menu_name: 'Salad',
+        menu_image: salad
+    },
+]
---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = ({category, setCategory}) => {
+
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Explore our menu and discover a delicious variety of dishes made to satisfy every craving. From sizzling fast food to healthy bites, we’ve got something for everyone. Dive in and treat yourself to your favorites—fresh, flavorful, and delivered to your door!</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index) =>{
+            return (
+                <div onClick={()=> setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+                    <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
