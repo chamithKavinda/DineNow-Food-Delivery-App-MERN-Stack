051bd63d380a17efb6dc22598e01d53e3a42b2c7
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-14 18:34:22 +0530

    feat: footer section implemented

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -13,6 +13,11 @@ import rating_starts from './rating_starts.png';
 import add_icon_white from './add_icon_white.png';
 import remove_icon_red from './remove_icon_red.png';
 import add_icon_green from './add_icon_green.png';
+import facebook_icon from './facebook_icon.png';
+import linkedin_icon from './linkedin_icon.png';
+import twitter_icon from './twitter_icon.png';
+import app_store from './app_store.png';
+import play_store from './play_store.png';
 
 import food_1 from './food-1.jpg';
 import food_2 from './food-2.jpg';
@@ -59,6 +64,11 @@ export const assets = {
     add_icon_white,
     remove_icon_red,
     add_icon_green,
+    facebook_icon,
+    linkedin_icon,
+    twitter_icon,
+    app_store,
+    play_store,
 };
 
 export const menu_list = [
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Dine Now is your go-to food delivery app, bringing your favorite meals right to your doorstep. Whether you're craving local flavors or popular dishes, we deliver fast, fresh, and reliably. With a simple interface and quick ordering, satisfying your hunger has never been easier. Enjoy delicious moments—anytime, anywhere.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+94 76 210 6834</li>
+                    <li>contact@dinenow.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">
+            Copyright 2025  DineNow.com - All rights Reserved
+        </p>
+    </div>
+  )
+}
+
+export default Footer
