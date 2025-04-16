5a7c079a98750fb947690e701e7c74f112285402
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-16 09:41:47 +0530

    feat: admin panel side bar , nav bar implemented

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Add.css'
+import {assets} from '../../assets/assets.js'
+
+const Add = () => {
+  return (
+    <div className='add'>
+      
+    </div>
+  )
+}
+
+export default Add
