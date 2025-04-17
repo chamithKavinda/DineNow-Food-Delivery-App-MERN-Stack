c4bbf9c7d5c8680fcf29fea32d28aac9fc70e588
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-17 19:09:05 +0530

    fix: url pass as a prop

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,6 +9,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+
+  const url = "http://localhost:4000";
+
   return (
     <div>
       <ToastContainer />
@@ -19,9 +22,9 @@ const App = () => {
         <Routes>
           {/* Redirect root path to list */}
           {/* <Route path="/" element={<Navigate to="" replace />} /> */}
-          <Route path="/add" element={<Add />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/orders" element={<Orders />} />
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
