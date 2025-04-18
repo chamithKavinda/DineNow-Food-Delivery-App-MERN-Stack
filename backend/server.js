a3e452605bd567d050fd0ed4b4d8b76386172286
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-19 01:19:46 +0530

    feat: order process implemented in backend

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,11 @@ import connectDB from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRouter.js';
 import cartRouter from './routes/cartRoute.js';
-import 'dotenv/config';
+import orderRouter from './routes/orderRoute.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 //app config
 const app = express();
@@ -22,6 +26,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static('uploads'));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get('/', (req, res) => {
     res.send('API Working');
