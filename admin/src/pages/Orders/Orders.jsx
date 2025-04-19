8b992309c3bb3e31331ed77a09e4b063d0bf493c
chamithkavinda <chamith13kavinda@gmail.com>
2025-04-19 18:25:40 +0530

    fix: order updating feature implemented

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -21,6 +21,16 @@ const Orders = ({url}) => {
     }
   }
 
+  const statusHandler = async (event,orderId) => {
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status: event.target.value
+    })
+    if(response.data.sucess){
+      await fetchAllOrders();
+    }
+  }
+
   useEffect(()=>{
     fetchAllOrders();
   },[])
@@ -54,7 +64,7 @@ const Orders = ({url}) => {
               </div>
               <p>Items : {order.items.length}</p>
               <p>LKR {order.amount}</p>
-              <select>
+              <select onChange={(event)=>statusHandler(event,order._id)} value={order.statusHandler}>
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out for Delivery">Out of Delivery</option>
                 <option value="Delivered">Delivered</option>
